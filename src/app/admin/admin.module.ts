5143eb0cf2ad8ae99c56f0a81afce22604e6eff4
Eugene <yukovich01@mail.ru>
2022-10-10 18:30:46 +0300

    Add Routing.

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,57 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AddPageComponent } from './add-page/add-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
+import { OrdersPageComponent } from './orders-page/orders-page.component';
+
+const adminRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/admin/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardPageComponent
+  },
+  {
+    path: 'add',
+    component: AddPageComponent
+  },
+  {
+    path: 'product/:id/edit',
+    component: EditPageComponent
+  },
+  {
+    path: 'orders',
+    component: OrdersPageComponent
+  },
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild([
+    {
+      path: '',
+      component: AdminLayoutComponent,
+      children: adminRoutes
+    }
+  ])],
+  exports: [RouterModule],
+  declarations: [
+    AdminLayoutComponent,
+    LoginPageComponent,
+    AddPageComponent,
+    DashboardPageComponent,
+    EditPageComponent,
+    OrdersPageComponent
+  ]
+})
+export class AdminModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const mainRoutes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent
+  },
+  {
+    path: 'product/:id',
+    component: ProductPageComponent
+  },
+  {
+    path: 'cart',
+    component: CartPageComponent
+  },
+];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: mainRoutes
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
